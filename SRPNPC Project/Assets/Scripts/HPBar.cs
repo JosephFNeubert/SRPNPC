850d3f584dbedcd186a4a22fb381857f15811edd
Joseph Neubert <joeyfneubert@gmail.com>
2025-11-07 00:49:20 -0700

    Assignment completed.

---
+++ SRPNPC Project/Assets/Scripts/HPBar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HPBar : MonoBehaviour
+{
+	public Slider _slider;
+
+	private void Start()
+	{
+		GetComponentInParent<IHealth>().OnHPPctChanged += HandleHPPctChanged;
+	}
+
+	private void HandleHPPctChanged(float pct)
+	{
+		Debug.Log("Hi");
+		_slider.value = pct;
+	}
+}
